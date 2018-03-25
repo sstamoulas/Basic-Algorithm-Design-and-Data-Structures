8a369a3dd4490c6d9fd6631ce6ccaa4a0c1e33e2
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2018-03-25 21:22:07 +0300

    Update MinHeap.java

--- MinHeap.java
+++ MinHeap.java
@@ -29,12 +29,46 @@ public class MinHeap {
 	 * @param val the value to be added to the heap
 	 */
 	public void add(CompareInt val) {
+		heap[++size] = val;
+		swim(heap, size);
+	}
+	
+	public void swim(CompareInt[] heap, int k) {
+		while(k > 1 && heap[k/2].compareTo(heap[k]) > 0) { 
+			swap(heap, k, k/2);
+			k = k/2;
+		}
 	}
 	
 	/**
 	 * Extracts the smallest element from the heap
 	 */
 	public CompareInt extractMin() {
-		return null;
+		CompareInt min = heap[1];
+		heap[1] = heap[size--];
+		sink(heap, 1);
+		
+		return min;
 	}
+	
+	public void swap(CompareInt[] heap, int k, int smallest) {
+		CompareInt temp = heap[k];
+		heap[k] = heap[smallest];
+		heap[smallest] = temp;
+	}
+	
+	public void sink(CompareInt[] heap, int k) {
+		while(2 * k <= size) {
+			int smallest = 2 * k;
+			if(heap[2 * k].compareTo(heap[2 * k + 1]) > 0) {
+				smallest = 2 * k + 1;
+			}
+			if(heap[k].compareTo(heap[smallest]) < 0 || smallest > size) {
+				break;
+			}
+			swap(heap, k, smallest);
+			k = smallest;
+		}
+	}
+	
 }
